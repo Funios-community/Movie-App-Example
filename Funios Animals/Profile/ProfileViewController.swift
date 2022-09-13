29f38fa2d85b4090e435701b0734315e4092af08
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-09-13 21:53:24 +0700

    Set profile username from saved data with userdefaults

--- Funios Animals/Profile/ProfileViewController.swift
+++ Funios Animals/Profile/ProfileViewController.swift
@@ -8,19 +8,25 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    var username: String?
     
     @IBOutlet weak var labelUsername: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        labelUsername.text = username
+        getUsernameAndSetItToWelcomeLabel()
+    }
+    
+    private func getUsernameAndSetItToWelcomeLabel() {
+        let usernameUserdefaultsKey = "com.funios.usernameKey"
+        let savedUsername = UserDefaults.standard.string(forKey: usernameUserdefaultsKey)!
+        labelUsername.text = "Hello, \(savedUsername)"
     }
 
     @IBAction func onLogoutClick(_ sender: UIButton) {
         self.tabBarController?.dismiss(animated: true)
+        UserDefaults.standard.set(false, forKey: "com.funios.loggedInkey")
     }
     
 }
